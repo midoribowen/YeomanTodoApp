d3b943a2583eac618ded9a7e998e54cfebc2905f
Midori Bowen <midori.p.bowen@gmail.com>
2016-04-29 13:22:43 -0700

    Add error messages using ng-messages.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngSanitize',
     'ngTouch',
     'ui.sortable',
-    'LocalStorageModule'
+    'LocalStorageModule',
+    'ngMessages'
   ])
   .config(['localStorageServiceProvider', function(localStorageServiceProvider){
     localStorageServiceProvider.setPrefix('ls');
